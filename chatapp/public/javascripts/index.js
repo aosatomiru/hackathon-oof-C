000ff12beb56c77efe784da490d862aeae9db176
teraomoyu <moyu_sno.234@ezweb.ne.jp>
2021-11-18 19:59:15 +0900

    チャットの部分の投稿機能

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -3,7 +3,7 @@
 // チャットルームに入室する
 function enter() {
     // 入力されたユーザ名を取得する
-    const userName = $('#userName').val();
+    const userName = $('#inputUserName').val();
     // ユーザ名が未入力でないかチェックする
     if('' === userName){
         alert('ユーザ名を入力してください');
