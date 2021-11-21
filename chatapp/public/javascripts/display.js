24ed83fb6c7bb9ee0ee4c3d0793f089a377e5e13
himekuri <himekuri9@gmail.com>
2021-11-22 03:16:32 +0900

    スライド表示の実装

---
+++ chatapp/public/javascripts/display.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// 「回答を見る」ボタンを押すと、#theme-area,#answer-areaに.open-answerを追加
+function openAnswer(){
+	let themeArea = document.getElementById("theme-area");
+	themeArea.classList.add("open-answer");
+	let answerArea = document.getElementById("answer-area");
+	answerArea.classList.add("open-answer");
+}
+
+function closeAnswer(){
+	let themeArea = document.getElementById("theme-area");
+	themeArea.classList.remove("open-answer");
+	let answerArea = document.getElementById("answer-area");
+	answerArea.classList.remove("open-answer");
+}
