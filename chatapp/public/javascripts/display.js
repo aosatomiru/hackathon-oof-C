e1f33a65e8afe59b2adb2069a49a16cf5a6582a3
himekuri <himekuri9@gmail.com>
2021-11-24 11:43:07 +0900

    回答ルームにお題を表示・回答保存されるように修正

--- chatapp/public/javascripts/display.js
+++ chatapp/public/javascripts/display.js
@@ -1,11 +1,16 @@
 'use strict';
 
 // 「回答を見る」ボタンを押すと、#theme-area,#answer-areaに.open-answerを追加
-function openAnswer(){
+function openAnswer(idNumber){
 	let themeArea = document.getElementById("theme-area");
 	themeArea.classList.add("open-answer");
 	let answerArea = document.getElementById("answer-area");
 	answerArea.classList.add("open-answer");
+
+	// 回答ルームにお題を表示
+	document.getElementById("re-theme").textContent = document.getElementById(idNumber + '-content').textContent;
+
+
 }
 // アイコンを押すと、#theme-area,#answer-areaから.open-answerを削除
 function closeAnswer(){
